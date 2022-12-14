95183a3759a63488d7fbb1150489893c0b743239
johyun1001 <115516629+johyun1001@users.noreply.github.com>
2022-12-14 11:06:18 +0900

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -20,10 +20,10 @@ sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'sphinx-hoverxref'
+project = 'Develop with Moreh Docs'
 year = datetime.datetime.now().year
-copyright = f'{year}, Manuel Kaufmann'
-author = 'Manuel Kaufmann'
+copyright = f'{year}, JH'
+author = 'JH'
 
 # The short X.Y version
 version = ''
