f88f4e2316c851cec8253b2f7dcd8800b3c70ed1
johyun1001 <115516629+johyun1001@users.noreply.github.com>
2023-03-27 15:27:46 +0900

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,6 @@ extensions = [
     "hoverxref.extension",
     "myst_parser",
     "notfound.extension",
-    "sphinx_design",
     "sphinx_search.extension",
     "sphinx_tabs.tabs",
     "sphinx-prompt",
