7ee264202e2c811741858db25febca7616da22f3
johyun1001 <115516629+johyun1001@users.noreply.github.com>
2023-03-27 15:13:08 +0900

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,264 +1,236 @@
-# -*- coding: utf-8 -*-
-#
-# Configuration file for the Sphinx documentation builder.
-#
-# This file does only contain a selection of the most common options. For a
-# full list see the documentation:
-# http://www.sphinx-doc.org/en/master/config
+"""
+Shared Sphinx configuration using sphinx-multiproject.
 
-# -- Path setup --------------------------------------------------------------
+To build each project, the ``PROJECT`` environment variable is used.
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
+.. code:: console
+
+   $ make html  # build default project
+   $ PROJECT=dev make html  # build the dev project
+
+for more information read https://sphinx-multiproject.readthedocs.io/.
+"""
+
 import os
-import datetime
 import sys
-sys.path.insert(0, os.path.abspath('..'))
 
+from multiproject.utils import get_project
 
-# -- Project information -----------------------------------------------------
-
-project = 'sphinx-hoverxref'
-year = datetime.datetime.now().year
-copyright = f'{year}, Manuel Kaufmann'
-author = 'Manuel Kaufmann'
-
-# The short X.Y version
-version = ''
-# The full version, including alpha/beta/rc tags
-release = ''
-
-
-# -- General configuration ---------------------------------------------------
-
-# If your documentation needs a minimal Sphinx version, state it here.
-#
-# needs_sphinx = '1.0'
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+sys.path.append(os.path.abspath("_ext"))
 extensions = [
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.autosectionlabel',
-    'sphinx.ext.mathjax',
-    'sphinx_tabs.tabs',
-    'sphinx-prompt',
-    'autoapi.extension',
-    'hoverxref.extension',
-    'versionwarning.extension',
-    'notfound.extension',
-    'sphinxcontrib.bibtex',
-    'sphinxemoji.sphinxemoji',
+    "hoverxref.extension",
+    "multiproject",
+    "myst_parser",
+    "notfound.extension",
+    "sphinx_design",
+    "sphinx_search.extension",
+    "sphinx_tabs.tabs",
+    "sphinx-prompt",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.intersphinx",
+    "sphinxcontrib.httpdomain",
+    "sphinxcontrib.video",
+    "sphinxemoji.sphinxemoji",
+    "sphinxext.opengraph",
 ]
 
-bibtex_bibfiles = ['refs.bib']
+multiproject_projects = {
+    "user": {
+        "use_config_file": False,
+        "config": {
+            "project": "Read the Docs user documentation",
+        },
+    },
+    "dev": {
+        "use_config_file": False,
+        "config": {
+            "project": "Read the Docs developer documentation",
+        },
+    },
+}
 
+docset = get_project(multiproject_projects)
+
+ogp_site_name = "Read the Docs Documentation"
+ogp_use_first_image = True  # https://github.com/readthedocs/blog/pull/118
+ogp_image = "https://docs.readthedocs.io/en/latest/_static/img/logo-opengraph.png"
+# Inspired by https://github.com/executablebooks/MyST-Parser/pull/404/
+ogp_custom_meta_tags = [
+    '<meta name="twitter:card" content="summary_large_image" />',
+]
+ogp_enable_meta_description = True
+ogp_description_length = 300
+
+templates_path = ["_templates"]
+
+master_doc = "index"
+copyright = "Read the Docs, Inc & contributors"
+version = "9.9.1"
+release = version
+exclude_patterns = ["_build", "shared", "_includes"]
+default_role = "obj"
+intersphinx_cache_limit = 14  # cache for 2 weeks
+intersphinx_timeout = 3  # 3 seconds timeout
 intersphinx_mapping = {
-    'readthedocs': ('https://docs.readthedocs.io/en/stable/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-    'sympy': ('https://docs.sympy.org/latest/', None),
-    'numpy': ('https://numpy.org/doc/stable/', None),
-    'python': ('https://docs.python.org/3/', None),
+    "python": ("https://docs.python.org/3.10/", None),
+    "django": (
+        "https://docs.djangoproject.com/en/stable/",
+        "https://docs.djangoproject.com/en/stable/_objects/",
+    ),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+    "pip": ("https://pip.pypa.io/en/stable/", None),
+    "nbsphinx": ("https://nbsphinx.readthedocs.io/en/latest/", None),
+    "myst-nb": ("https://myst-nb.readthedocs.io/en/stable/", None),
+    "ipywidgets": ("https://ipywidgets.readthedocs.io/en/stable/", None),
+    "jupytext": ("https://jupytext.readthedocs.io/en/stable/", None),
+    "ipyleaflet": ("https://ipyleaflet.readthedocs.io/en/latest/", None),
+    "poliastro": ("https://docs.poliastro.space/en/stable/", None),
+    "qiskit": ("https://qiskit.org/documentation/", None),
+    "myst-parser": ("https://myst-parser.readthedocs.io/en/stable/", None),
+    "writethedocs": ("https://www.writethedocs.org/", None),
+    "jupyterbook": ("https://jupyterbook.org/en/stable/", None),
+    "executablebook": ("https://executablebooks.org/en/latest/", None),
+    "rst-to-myst": ("https://rst-to-myst.readthedocs.io/en/stable/", None),
+    "rtd": ("https://docs.readthedocs.io/en/stable/", None),
+    "rtd-dev": ("https://dev.readthedocs.io/en/latest/", None),
+    "jupyter": ("https://docs.jupyter.org/en/latest/", None),
 }
+
+# Intersphinx: Do not try to resolve unresolved labels that aren't explicitly prefixed.
+# The default setting for intersphinx_disabled_reftypes can cause some pretty bad
+# breakage because we have rtd and rtd-dev stable versions in our mappings.
+# Hence, if we refactor labels, we won't see broken references, since the
+# currently active stable mapping keeps resolving.
+# Recommending doing this on all projects with Intersphinx.
+# https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html#confval-intersphinx_disabled_reftypes
+intersphinx_disabled_reftypes = ["*"]
+
+myst_enable_extensions = [
+    "deflist",
+]
 hoverxref_intersphinx = [
-    'readthedocs',
-    'sphinx',
-    'sympy',
-    'numpy',
-    'python',
+    "sphinx",
+    "pip",
+    "nbsphinx",
+    "myst-nb",
+    "ipywidgets",
+    "jupytext",
 ]
-hoverxref_intersphinx_types = {
-    'readthedocs': 'modal',
-    'sphinx': 'tooltip',
-}
-
-# Used when building the documentation from the terminal and using a local Read
-# the Docs instance as backend
-hoverxref_api_host = 'http://localhost:8000'
-
-if os.environ.get('READTHEDOCS') == 'True':
-    # Building on Read the Docs
-    hoverxref_api_host = 'https://readthedocs.org'
-
-    if os.environ.get('PROXIED_API_ENDPOINT') == 'True':
-        # Use the proxied API endpoint
-        hoverxref_api_host = '/_'
-
-if os.environ.get('LOCAL_READTHEDOCS') == 'True':
-    # Building on a local Read the Docs instance
-    hoverxref_api_host = 'http://community.dev.readthedocs.io'
-
-if os.environ.get('NGROK_READTHEDOCS') == 'True':
-    # Building on a local Read the Docs instance using NGROK for HTTPS
-    hoverxref_api_host = 'https://readthedocs.ngrok.io'
-
-hoverxref_tooltip_maxwidth = 650
-hoverxref_auto_ref = True
-hoverxref_roles = [
-    'confval',
-    'term',
+htmlhelp_basename = "ReadTheDocsdoc"
+latex_documents = [
+    (
+        "index",
+        "ReadTheDocs.tex",
+        "Read the Docs Documentation",
+        "Eric Holscher, Charlie Leifer, Bobby Grace",
+        "manual",
+    ),
+]
+man_pages = [
+    (
+        "index",
+        "read-the-docs",
+        "Read the Docs Documentation",
+        ["Eric Holscher, Charlie Leifer, Bobby Grace"],
+        1,
+    )
 ]
 
-hoverxref_role_types = {
-    'hoverxref': 'tooltip',
-    'ref': 'modal',
-    'confval': 'tooltip',
-    'mod': 'modal',
-    'class': 'modal',
-    'obj': 'tooltip',
-}
-hoverxref_domains = [
-    'py',
-    'cite',
-]
-hoverxref_sphinxtabs = True
-hoverxref_mathjax = True
-
-autosectionlabel_prefix_document = True
-
-autoapi_dirs = ['../hoverxref']
-autoapi_add_toctree_entry = False
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-
-# The suffix(es) of source filenames.
-# You can specify multiple suffix as a list of string:
-#
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
-
-# The master toctree document.
-master_doc = 'index'
-
-# The language for content autogenerated by Sphinx. Refer to documentation
-# for a list of supported languages.
-#
-# This is also used if you do content translation via gettext catalogs.
-# Usually you set "language" from the command line for these cases.
 language = "en"
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+locale_dirs = [
+    f"{docset}/locale/",
+]
+gettext_compact = False
 
-# The name of the Pygments (syntax highlighting) style to use.
-pygments_style = None
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static", f"{docset}/_static"]
+html_css_files = ["css/custom.css", "css/sphinx_prompt_css.css"]
+html_js_files = ["js/expand_tabs.js"]
 
+if os.environ.get("READTHEDOCS_VERSION_TYPE") == "external":
+    html_js_files.append("js/readthedocs-doc-diff.js")
 
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'sphinx_rtd_theme'
-
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#
-# html_theme_options = {}
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
-
-# Custom sidebar templates, must be a dictionary that maps document names
-# to template names.
-#
-# The default sidebars (for documents that don't match any pattern) are
-# defined by theme itself.  Builtin themes are using these templates by
-# default: ``['localtoc.html', 'relations.html', 'sourcelink.html',
-# 'searchbox.html']``.
-#
-# html_sidebars = {}
-
-
-# -- Options for HTMLHelp output ---------------------------------------------
-
-# Output file base name for HTML help builder.
-htmlhelp_basename = 'sphinx-hoverxrefdoc'
-
-
-# -- Options for LaTeX output ------------------------------------------------
-
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
-
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
-
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
-
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
+html_logo = "img/logo.svg"
+html_theme_options = {
+    "logo_only": True,
+    "display_version": False,
+}
+html_context = {
+    # Fix the "edit on" links.
+    # TODO: remove once we support different rtd config
+    # files per project.
+    "conf_py_path": f"/docs/{docset}/",
+    # Use to generate the Plausible "data-domain" attribute from the template
+    "plausible_domain": f"{os.environ.get('READTHEDOCS_PROJECT')}.readthedocs.io",
 }
 
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, 'sphinx-hoverxref.tex', 'sphinx-hoverxref Documentation',
-     'Manuel Kaufmann', 'manual'),
+hoverxref_auto_ref = True
+hoverxref_domains = ["py"]
+hoverxref_roles = [
+    "option",
+    # Documentation pages
+    # Not supported yet: https://github.com/readthedocs/sphinx-hoverxref/issues/18
+    "doc",
+    # Glossary terms
+    "term",
+]
+hoverxref_role_types = {
+    "mod": "modal",  # for Python Sphinx Domain
+    "doc": "modal",  # for whole docs
+    "class": "tooltip",  # for Python Sphinx Domain
+    "ref": "tooltip",  # for hoverxref_auto_ref config
+    "confval": "tooltip",  # for custom object
+    "term": "tooltip",  # for glossaries
+}
+
+# See dev/style_guide.rst for documentation
+rst_epilog = """
+.. |org_brand| replace:: Read the Docs Community
+.. |com_brand| replace:: Read the Docs for Business
+.. |git_providers_and| replace:: GitHub, Bitbucket, and GitLab
+.. |git_providers_or| replace:: GitHub, Bitbucket, or GitLab
+"""
+
+# Activate autosectionlabel plugin
+autosectionlabel_prefix_document = True
+
+# sphinx-notfound-page
+# https://github.com/readthedocs/sphinx-notfound-page
+notfound_context = {
+    "title": "Page Not Found",
+    "body": """
+<h1>Page Not Found</h1>
+
+<p>Sorry, we couldn't find that page.</p>
+
+<p>Try using the search box or go to the homepage.</p>
+""",
+}
+linkcheck_retries = 2
+linkcheck_timeout = 1
+linkcheck_workers = 10
+linkcheck_ignore = [
+    r"http://127\.0\.0\.1",
+    r"http://localhost",
+    r"http://community\.dev\.readthedocs\.io",
+    r"https://yourproject\.readthedocs\.io",
+    r"https?://docs\.example\.com",
+    r"https://foo\.readthedocs\.io/projects",
+    r"https://github\.com.+?#L\d+",
+    r"https://github\.com/readthedocs/readthedocs\.org/issues",
+    r"https://github\.com/readthedocs/readthedocs\.org/pull",
+    r"https://docs\.readthedocs\.io/\?rtd_search",
+    r"https://readthedocs\.org/search",
+    # This page is under login
+    r"https://readthedocs\.org/accounts/gold",
 ]
 
+extlinks = {
+    "rtd-issue": ("https://github.com/readthedocs/readthedocs.org/issues/%s", "#%s"),
+}
 
-# -- Options for manual page output ------------------------------------------
-
-# One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'sphinx-hoverxref', 'sphinx-hoverxref Documentation',
-     [author], 1)
-]
-
-
-# -- Options for Texinfo output ----------------------------------------------
-
-# Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-texinfo_documents = [
-    (master_doc, 'sphinx-hoverxref', 'sphinx-hoverxref Documentation',
-     author, 'sphinx-hoverxref', 'One line description of project.',
-     'Miscellaneous'),
-]
-
-
-# -- Options for Epub output -------------------------------------------------
-
-# Bibliographic Dublin Core info.
-epub_title = project
-
-# The unique identifier of the text. This can be a ISBN number
-# or the project homepage.
-#
-# epub_identifier = ''
-
-# A unique identification for the text.
-#
-# epub_uid = ''
-
-# A list of files that should not be packed into the epub file.
-epub_exclude_files = ['search.html']
-
-
-def setup(app):
-    app.add_object_type(
-        'confval',  # directivename
-        'confval',  # rolename
-        'pair: %s; configuration value',  # indextemplate
-    )
-
-    app.add_css_file('css/custom.css')
+# Disable epub mimetype warnings
+suppress_warnings = ["epub.unknown_project_files"]
