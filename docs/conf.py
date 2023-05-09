6058e1e5151a15dfa6f24c3f8d94713deacab35b
johyun1001 <115516629+johyun1001@users.noreply.github.com>
2023-05-09 23:26:02 +0900

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -66,6 +66,11 @@ myst_heading_anchors = 3
 #
 # -- Options for HTML output -------------------------------------------------
 #
+html_theme = "sphinx_rtd_theme"
 
-html_theme = "furo"
-html_title = project
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
+
+
